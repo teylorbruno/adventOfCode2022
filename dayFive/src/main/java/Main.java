87b7ece87f6331bde5ba74647e5313e4887aa533
Bruno Domingues <teylorbruno@gmail.com>
2022-12-08 21:10:20 +0000

    day 5 solved dev

--- dayFive/src/main/java/Main.java
+++ dayFive/src/main/java/Main.java
@@ -19,10 +19,13 @@ public class Main {
 			int quantity = instruction.get(0);
 			int originColumn = instruction.get(1);
 			int destinationColumn = instruction.get(2);
+			LinkedList<String> l = new LinkedList<>();
 			for (int i = 1; i <= quantity; i++) {
 				String removed = grid.get(originColumn).pop();
-				grid.get(destinationColumn).push(removed);
+				l.addLast(removed);
+				//grid.get(destinationColumn).push(removed);
 			}
+			grid.get(destinationColumn).addAll(0, l);
 		});
 	}
 }
